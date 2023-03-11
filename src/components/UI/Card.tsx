88b0ff2f3278996e36488ab9b1294a2a3c2428fc
Gregory Delpha <delpha.gregory@gmail.com>
2023-03-11 18:50:27 +0100

    import react

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export type ChildrenProps = {
 	children: React.ReactNode;
 };
--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChangeEvent } from 'react';
 import { Card } from '../UI/Card';
 

02043802b871beee30b89eb43b683a217596e697
Gregory Delpha <delpha.gregory@gmail.com>
2023-03-11 19:07:09 +0100

    2nd test

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -30,7 +30,7 @@ export function AddUser() {
 						htmlFor="note"
 						className="block text-gray-700 text-sm font-bold mb-2"
 					>
-						Contenu TEST
+						Contenu
 					</label>
 					<textarea
 						id="note"

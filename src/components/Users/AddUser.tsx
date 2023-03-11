e1cc58edd0a815746fac5ddf6f10bf88097bbf51
Gregory Delpha <delpha.gregory@gmail.com>
2023-03-11 19:11:53 +0100

    test commit

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -42,7 +42,7 @@ export function AddUser() {
 						type="submit"
 						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 					>
-						Valider
+						Enregistrer
 					</button>
 				</div>
 			</form>

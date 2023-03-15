d3c55e27573ae4295d83d7242352d14afa30f33e
gregmaax <greg.d.972@gmail.com>
2023-03-15 10:57:20 +0100

    modif affichage + storage des valeurs entrées

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,51 +1,73 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import Button from '../UI/Button';
 import { Card } from '../UI/Card';
 
 export function AddUser() {
+	const [titre, setTitre] = useState('');
+	const [note, setNote] = useState('');
+
 	function addNoteHandler(event: ChangeEvent<HTMLFormElement>) {
 		event.preventDefault();
+
+		if (titre.trim().length === 0 || note.trim().length === 0) {
+			return;
+		}
+
+		console.log(titre, note);
+		setTitre('');
+		setNote('');
 	}
+
+	function titreChangeHandler(event: ChangeEvent<HTMLInputElement>) {
+		setTitre(event.target.value);
+	}
+
+	function noteChangeHandler(event: ChangeEvent<HTMLTextAreaElement>) {
+		setNote(event.target.value);
+	}
+
 	return (
-		<Card>
-			<form
-				onSubmit={addNoteHandler}
-				className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-			>
-				<div className="mb-4">
-					<label
-						htmlFor="titre"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Titre de la note
-					</label>
-					<input
-						id="titre"
-						type="text"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="note"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Contenu de la note
-					</label>
-					<textarea
-						id="note"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="text-right">
-					<Button
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-						type="submit"
-					>
-						Enregistrer
-					</Button>
-				</div>
-			</form>
-		</Card>
+		<form
+			onSubmit={addNoteHandler}
+			className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md"
+		>
+			<div className="mb-4">
+				<label
+					htmlFor="titre"
+					className="block text-gray-700 text-sm font-bold mb-2"
+				>
+					Titre de la note
+				</label>
+				<input
+					id="titre"
+					type="text"
+					onChange={titreChangeHandler}
+					value={titre}
+					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+				/>
+			</div>
+			<div className="mb-4">
+				<label
+					htmlFor="note"
+					className="block text-gray-700 text-sm font-bold mb-2"
+				>
+					Contenu de la note
+				</label>
+				<textarea
+					id="note"
+					onChange={noteChangeHandler}
+					value={note}
+					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+				/>
+			</div>
+			<div className="text-right">
+				<Button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					type="submit"
+				>
+					Enregistrer
+				</Button>
+			</div>
+		</form>
 	);
 }
